b86561aba9c0da2de12ac14e54ee3755aaaf6993
Pravin Ramana <pravinramana25@protonmail.ch>
2022-10-16 15:37:59 -0500

    Show download status with indicatif

--- src/common.rs
+++ src/common.rs
@@ -11,13 +11,18 @@ use tokio::io::AsyncWriteExt;
 pub async fn download_into<U, P>(url: U, location: P) -> Result<(), Box<dyn std::error::Error>>
 where
     U: reqwest::IntoUrl,
-    P: AsRef<std::path::Path>,
+    P: AsRef<std::path::Path> + std::fmt::Display,
 {
-    let mut file = tokio::fs::File::create(location).await?;
+    let mut file = tokio::fs::File::create(&location).await?;
     let mut stream = crate::common::CLIENT.get(url).send().await?.bytes_stream();
+
+    let bar = indicatif::ProgressBar::new_spinner();
+    bar.set_message(format!("Downloading {}", &location));
     while let Some(chunk) = stream.next().await {
         let bytes = chunk?;
         file.write_all(&bytes).await?;
+        bar.tick();
     }
+    bar.finish();
     Ok(())
 }
