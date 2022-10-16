4e207443b20f6e87605872d0c9d0c0be2c126070
Pravin Ramana <pravinramana25@protonmail.ch>
2022-10-16 14:34:25 -0500

    Code for writing flv and saving constants

---
+++ src/common.rs
@@ -0,0 +1,23 @@
+lazy_static::lazy_static! {
+    pub static ref CLIENT: reqwest::Client = reqwest::Client::new();
+}
+
+pub const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36";
+pub const COOKIE: &str = env!("TIKTOK_COOKIE");
+
+use futures::stream::StreamExt;
+use tokio::io::AsyncWriteExt;
+
+pub async fn download_into<U, P>(url: U, location: P) -> Result<(), Box<dyn std::error::Error>>
+where
+    U: reqwest::IntoUrl,
+    P: AsRef<std::path::Path>,
+{
+    let mut file = tokio::fs::File::create(location).await?;
+    let mut stream = crate::common::CLIENT.get(url).send().await?.bytes_stream();
+    while let Some(chunk) = stream.next().await {
+        let bytes = chunk?;
+        file.write_all(&bytes).await?;
+    }
+    Ok(())
+}
